f9470200ce7a60a988cc8a72552393550c23b52a
Dilsekh Patnaik <98086314+dilsekhpatnaik7@users.noreply.github.com>
2022-05-23 22:37:37 +0530

    Time: 4 ms (67.91%), Space: 11.1 MB (75.78%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        auto it=find(nums.begin(),nums.end(),target);
+        if(it!=nums.end())
+            return it-nums.begin();
+        else
+            return -1;
+    }
+};
