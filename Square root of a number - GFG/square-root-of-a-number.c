0c9f31c427c97eda6196ef65ef72d390e8924675
Dilsekh Patnaik <98086314+dilsekhpatnaik7@users.noreply.github.com>
2022-06-23 21:12:31 +0530

    Added solution - LeetHub

---
+++ Square root of a number - GFG/square-root-of-a-number.c
@@ -0,0 +1,31 @@
+// { Driver Code Starts
+//Initial Template for C
+
+#include<stdio.h>
+  
+
+ // } Driver Code Ends
+//User function Template for C
+
+long long int floorSqrt(long long int x) 
+{
+    // Your code goes here  
+    return sqrt(x);
+}
+
+// { Driver Code Starts.
+
+int main()
+{
+	int t;
+	scanf("%d", &t);
+	while(t--)
+	{
+		long long n;
+		scanf("%ld", &n);
+	
+		printf("%ld\n", floorSqrt(n));
+	}
+    return 0;   
+}
+  // } Driver Code Ends
