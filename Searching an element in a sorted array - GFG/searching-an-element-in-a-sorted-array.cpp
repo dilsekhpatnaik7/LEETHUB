838f403ac504eee1c81b6603a7d0df636d0d3bcb
Dilsekh Patnaik <98086314+dilsekhpatnaik7@users.noreply.github.com>
2023-02-21 00:44:37 +0530

    Added solution - LeetHub

--- Searching an element in a sorted array - GFG/searching-an-element-in-a-sorted-array.cpp
+++ Searching an element in a sorted array - GFG/searching-an-element-in-a-sorted-array.cpp
@@ -7,8 +7,12 @@ using namespace std;
 class Solution{
 public:
     int searchInSorted(int arr[], int N, int K) {
-        for(int i = 0; i < N; i++) {
-            if(arr[i] == K) return 1;
+        int low = 0, high = N - 1;
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            if(arr[mid] == K) return 1;
+            else if(K > arr[mid]) low = mid + 1;
+            else high = mid - 1;
         }
         return -1;
     }
