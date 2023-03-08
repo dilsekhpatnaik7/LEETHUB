e8393ce1064c1300bea098f1743065697301263d
Dilsekh Patnaik <98086314+dilsekhpatnaik7@users.noreply.github.com>
2023-03-08 20:26:48 +0530

    Added solution - LeetHub

---
+++ Max min Height - GFG/max-min-height.cpp
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+public:
+    bool isOK(vector<int> &arr, int days, int cs, long long int h){
+        int n = arr.size();
+        vector<long long> waterSupply(n,0);
+        if(arr[0] < h){
+            waterSupply[0] = h - arr[0];
+            days -= (h-arr[0]);
+        }
+        if(days < 0){
+            return false;
+        }
+        for(int i = 1; i < arr.size(); i++){
+            waterSupply[i] = waterSupply[i - 1];
+            int actualHeight = arr[i];
+            if(i >= cs){
+                actualHeight += (waterSupply[i] - waterSupply[i - cs]);
+            }
+            else{
+                actualHeight += waterSupply[i];
+            }
+            if(actualHeight < h){
+                waterSupply[i] += (h - actualHeight);
+                days -= (h-actualHeight);
+            }
+            if(days < 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    long long int maximizeMinHeight(int n,int k,int w,vector <int> &a){
+        long long int res = -1;
+        long long int mnHeight = *min_element(a.begin(), a.end());
+        long long int mxHeight = INT_MAX/2;
+        while(mnHeight <= mxHeight){
+            int guessHeight = (mxHeight + mnHeight) / 2;
+            if(isOK(a,k,w,guessHeight)){
+                res = guessHeight;
+                mnHeight = guessHeight + 1;
+            }
+            else{
+                mxHeight = guessHeight - 1;
+            }
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k,w;
+        cin>>n>>k>>w;
+        vector <int> a(n);
+        for(auto &j:a)
+            cin>>j;
+        Solution s;
+        cout<<s.maximizeMinHeight(n,k,w,a)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
