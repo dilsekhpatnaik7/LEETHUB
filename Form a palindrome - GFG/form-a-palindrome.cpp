834c82e02f63b373f79d3453ee3b70253501e6de
Dilsekh Patnaik <98086314+dilsekhpatnaik7@users.noreply.github.com>
2023-11-04 12:33:03 +0530

    Added solution - LeetHub

---
+++ Form a palindrome - GFG/form-a-palindrome.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    int solve(int n, int m, string s, string t, vector<vector<int>>& dp) {
+    	for(int i = 0; i <= n; i++) {
+            for(int j = 0; j <= m; j++) {
+                if(i == 0 || j == 0) dp[i][j] = 0;
+                else if(s[i - 1] == t[j - 1]) dp[i][j] = 1 + dp[i - 1][j - 1];
+                else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[n][m];
+    }
+  
+    int countMin(string str) {
+        int n = str.size();
+    	string t = str;
+    	reverse(t.begin(), t.end());
+    
+    	vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+    	return n - solve(n, n, str, t, dp);
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        string str;
+        cin >> str;
+        Solution ob;
+        cout << ob.countMin(str) << endl;
+    }
+return 0;
+}
+
+
+// } Driver Code Ends
